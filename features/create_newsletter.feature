914362675623323625c3f7301bd44be804be765a
Andre Heijstek <andre.heijstek@improvementfocus.com>
2015-12-18 22:16:13 +0100

    Most of the newsletter.feature works, now to check the content of the JSON page

--- features/create_newsletter.feature
+++ features/create_newsletter.feature
@@ -1,6 +1,16 @@
-Feature: 
-  In order to 
-  As a 
-  I want 
+Feature: Create a newsletter
+  In order to publish to Word and App
+  As a newsletter editor
+  I want to create a newsletter with all attributes required
+
+  Scenario Outline: Create a very simple newsletter
+    Given I login as "test.user@test.nl" with password "supersecret"
+    When I enter text on "<welcome>", date as <year>, "<month>", <day>, time1 as <hour1>, <minute1>, time2 as <hour2>, <minute2>, "<announcement>" and "<liturgy>"
+    Then I want to see a confirmation message
+    And I want to see the same data in the JSON page
+    Examples:
+      | welcome | year | month | day | hour1 | minute1 | hour2 | minute2 | announcement | liturgy  |
+      | hallo   | 2015 | May   | 12  | 10    | 00      | 17    | 00      | mededeling   | liturgie |
+    
 
   
---
+++ features/step_definitions/newsletter.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+Before do
+  @url = "http://127.0.0.1:3000"
+  end
+
+When(/^I enter text on "([^"]*)", date as (\d+), "([^"]*)", (\d+), time1 as (\d+), (\d+), time2 as (\d+), (\d+), "([^"]*)" and "([^"]*)"$/) do | welcome, year, month, day, hour1, minute1, hour2, minute2, announcement, liturgy |
+  visit @url
+  click_on('Zondagsbrieven')
+  click_on('New Letter')
+
+  fill_in('Welcome', :with => welcome)
+  select year, from: 'letter_date_1i'
+  select month, from: 'letter_date_2i'
+  select day, from: 'letter_date_3i'
+  select hour1, from: 'letter_time1_4i'
+  select minute1, from: 'letter_time1_5i'
+  select hour2, from: 'letter_time2_4i'
+  select minute2, from: 'letter_time2_5i'
+
+  fill_in('Announcements', :with => announcement)
+  fill_in('Liturgy', :with => liturgy)
+
+  click_button('Create Letter')
+end
+
+Then(/^I want to see a confirmation message$/) do
+  expect(page).to have_content('Letter was successfully created.')
+end
+
+Then(/^I want to see the same data in the JSON page$/) do
+  visit "#{@url}/letters.json"
+end
